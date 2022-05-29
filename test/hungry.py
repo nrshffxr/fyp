201c14655c1933df70113c09977d31be9d3bd407
nrshffxr <norisheffiixora02@gmail.com>
2022-05-29 07:38:55 -0700

    fries

--- test/hungry.py
+++ test/hungry.py
@@ -2,7 +2,7 @@ hungry=input("are you hungry?")
 if hungry=="yes":
     print("eat samosa")
     print("eat pizza")
-    print("eat burger")
+    print("eat fries")
 else:
     thirsty=input("are you thirsty?")
     if thirsty=="yes":
