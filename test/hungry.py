9837ee8e8cec2eca41b675c92ba1ba6899089292
nrshffxr <norisheffiixora02@gmail.com>
2022-05-29 07:35:07 -0700

    water

--- test/hungry.py
+++ test/hungry.py
@@ -4,4 +4,6 @@ if hungry=="yes":
     print("eat pizza")
     print("eat burger")
 else:
-    print("do your homework")
+    thirsty=input("are you thirsty?")
+    if thirsty=="yes":
+    print("drink water")
