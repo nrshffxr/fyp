087ead9b1227d48f75d5f17cd3006ee5347cbfed
nrshffxr <norisheffiixora02@gmail.com>
2022-06-06 03:45:13 -0700

    ice

--- hungry.py
+++ hungry.py
@@ -1,8 +1,10 @@
 hungry==input("are you hungry?")
 if hungry=="yes":
 print("eat pizza")
+print("eat ice")
 else:
 thirsty=input("are you thirsty?")
 if thirsty=="yes":
 print("drink water")
 print("drink milo")
+
