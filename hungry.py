7d25503167f33fe67660e45a8bdd1a177ca87f86
nrshffxr <norisheffiixora02@gmail.com>
2022-06-06 03:20:31 -0700

    sushi again

--- hungry.py
+++ hungry.py
@@ -2,9 +2,11 @@ hungry=input("are you hungry?")
 if hungry=="yes":
     print("eat rice")
     print("eat pizza")  
+    print("eat sushi")
 else:
    thirsty=input("are you thirsty?")
-   if thirsty=="yes":
-      print("drink water")
-      print("drink milo")
-    
+if thirsy=="yes":
+    print("drink water")
+
+
+
