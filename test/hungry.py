bca246e8b57cadcab8cfd89663f6255ffc79ceec
nrshffxr <norisheffiixora02@gmail.com>
2022-06-06 20:04:09 -0700

    back to ori file

--- test/hungry.py
+++ test/hungry.py
@@ -2,6 +2,4 @@ hungry=input("are you hungry?")
 if hungry=="yes":
     print("eat pizza")
 else:
-    print("do your hw")
-    print("watch drama lah")
-    print("play guitar")
+    print("watch drama")
