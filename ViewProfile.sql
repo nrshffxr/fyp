3c28f890193216ab417659cdbdcfa129f489d496
20026183-Akash <87625689+20026183-Akash@users.noreply.github.com>
2022-08-05 18:09:38 +0800

    Update ViewProfile.sql

--- ViewProfile.sql
+++ ViewProfile.sql
@@ -7,6 +7,9 @@
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
+DROP DATABASE ViewProfile2;
+CREATE DATABASE ViewProfile2;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
